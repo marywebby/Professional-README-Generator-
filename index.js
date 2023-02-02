a01cab752c17b5a47bf0162d0237233af003ce89
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-02-02 12:11:33 -0500

    Finished Project

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile } = require('fs').promises;
 
-// TODO: Create an array of questions for user input
-const questions = [
+
+//Created an array of questions for user input
+const questionsPrompt = () => {
+  return inquirer.prompt(
+        [
         {
         type: 'input',
-        name: 'project-title',
+        name: 'projectTitle',
         message: 'What is the title of your project?',
         },
         {
@@ -16,7 +19,7 @@ const questions = [
         },
         {
         type: 'input',
-        name: 'intallation',
+        name: 'installation',
         message: 'How should one install your project to use it?',
         },
         {
@@ -33,7 +36,7 @@ const questions = [
         type: 'list',
         message: 'What license would you like your project to use? ',
         name: 'license', 
-        choices: ['Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License'],
+        choices: ['Apache License 2.0', 'GNU General Public License v3.0', `Eclipse Public License 1.0`, 'MIT License', 'BSD 2-Clause "Simplified" License', `None`],
         },
         {
         type: 'input',
@@ -45,19 +48,94 @@ const questions = [
         name: 'email',
         message: 'Enter your email address',
         },
-  ]
+  ]);
+};
+
+
+// here the varibales for inputting the correct license will come from 
+const apache= `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+const gnu = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+const mit = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+const bsd = `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`
+const eclipse = `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`
+const none = ``
+
+// this is where the HTML is being set up for furture use when the function is called.
+const generateHTMLreadme = ({projectTitle, description, installation, usage, credits, license, github, email}) => 
+  `
+  # ${projectTitle}
+
+
+  ${inputLisenceBadge(license)}
+
+
+  ## Description
+      ${description}
+
+
+  ## Table of Contents
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Credits](#credits)
+
+
+  ## Installation
+      ${installation}
+
+
+  ## Usage
+      ${usage}
+
+
+  ## License
+      This project is licensed under the: ${license}
+
+
+  ## Credits
+      ${credits}
+
+
+##Questions
+      Github Username: @${github}
+      Github webpage: https://github.com/${github}
+      For any additional inquiries, please email at: ${email}
+`
 ;
+   
 
+// this function will render in the license badge that the user will have selected previously 
+function inputLisenceBadge (license) {
+  if (license === `Apache License 2.0`) {
+    return apache
+  }
+  if (license === `GNU General Public License v3.0`) {
+    return gnu
+  }
+  if (license === `Eclipse Public License 1.0`) {
+    return eclipse
+  }
+  if (license === `MIT License`) {
+    return mit
+  }
+  if (license === `BSD 2-Clause "Simplified" License`) {
+    return bsd
+  }
+  if (license === `None`) {
+    return none
+  }
+  return ``;
+};
 
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {
-    // console.log(questions);
-    inquirer.prompt(questions);
-}
+const init = () => {
+  questionsPrompt()
+    .then((answers) => writeFile("README.md", generateHTMLreadme(answers)))
+    .then(() => console.log("Sucsessfully created README.me!"))
+    .catch((err) => console.log(err));
+  };
+
 
 // Function call to initialize app
 init();
---
+++ prev.readme/README.md
@@ -0,0 +1,38 @@
+
+  # Professional README Generator 
+
+
+  
+
+
+  ## Description
+      My project will entail a functional in console way of easily genorating a customizable README.md file, in which a user will be able to efficently create the content which will go inside themselves! 
+
+
+  ## Table of Contents
+      - [Installation](#installation)
+      - [Usage](#usage)
+      - [License](#license)
+      - [Credits](#credits)
+
+
+  ## Installation
+      One must install nodejs, fs, and inquirer to properly run the project of their own computer! 
+
+
+  ## Usage
+       Please launch node.js following the line with the correct file name, which in this case is `index.js`, and fill in the answers in the command line. 
+
+
+  ## License
+      This project is licensed under the: None
+
+
+  ## Credits
+      MSU Coding Bootcamp, and my instructor Brad Dunn, and my TA Grady Peck.
+
+
+##Questions
+      Github Username: @marywebby
+      Github webpage: https://github.com/marywebby
+      For any additional inquiries, please email at: marywebby1999@gmail.com 
